86da9672bc5b73dccf403c222b8425a248e88b3e
Nimesh Srivastava <snimesh412@gmail.com>
2025-03-01 10:14:36 +0530

    Home screen complete

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,42 +1,14 @@
-import { Text, View, Image, StyleSheet } from "react-native";
-import { formatDistanceToNowStrict } from "date-fns";
+import { FlatList, View } from "react-native";
 import posts from "../../../assets/data/posts.json";
+import PostListItem from "../../components/PostListItem";
 
 export default function HomeScreen() {
-    const post = posts[0];
     return (
-        <View
-            style={{
-                paddingHorizontal: 15,
-                paddingVertical: 10,
-            }}
-        >
-            {/* post header */}
-            <View style={{ flexDirection: "row", gap: 10 }}>
-                <Image source={{ uri: post.group.image }} style={styles.image} />
-                <Text style={{ fontWeight: "bold" }}>{post.group.name}</Text>
-                <Text style={{ color: "grey" }}>
-                    {" "}
-                    {formatDistanceToNowStrict(new Date(post.created_at))}
-                </Text>
-                <View style={{ marginLeft: "auto" }}>
-                    <Text style={styles.joinButtonText}>Join</Text>
-                </View>
-            </View>
-
-            {/* post content */}
+        <View>
+            <FlatList
+                data={posts}
+                renderItem={({ item }) => <PostListItem post={item} />}
+            />
         </View>
     );
 }
-
-const styles = StyleSheet.create({
-    joinButtonText: {
-        backgroundColor: "#4c00cf",
-        color: "white",
-        paddingVertical: 2,
-        paddingHorizontal: 7,
-        borderRadius: 10,
-        fontWeight: "bold",
-    },
-    image: { width: 20, height: 20, borderRadius: 10 },
-});
