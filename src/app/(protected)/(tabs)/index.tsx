cf5f979252d81030b1d7b8d955635218016946e4
Nimesh Srivastava <snimesh412@gmail.com>
2025-03-02 22:58:27 +0530

    Fetch data from supabase db

--- src/app/(protected)/(tabs)/index.tsx
+++ src/app/(protected)/(tabs)/index.tsx
@@ -1,15 +1,33 @@
 import { FlatList, View } from "react-native";
-import posts from "../../../../assets/data/posts.json";
+//import posts from "../../../../assets/data/posts.json";
 import PostListItem from "../../../components/PostListItem";
+import { supabase } from "../../../lib/supabase";
+import { useEffect, useState } from "react";
 
 export default function HomeScreen() {
-  return (
-    <View style={{ backgroundColor: "#101218" }}>
-      <FlatList
-        showsVerticalScrollIndicator={false}
-        data={posts}
-        renderItem={({ item }) => <PostListItem post={item} />}
-      />
-    </View>
-  );
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        fetchPosts();
+    }, []);
+
+    const fetchPosts = async () => {
+        const { data, error } = await supabase
+            .from("posts")
+            .select("*, group:groups(*), user:users!posts_user_id_fkey(*)");
+        console.log(error);
+        console.log("data : ", data);
+
+        setPosts(data);
+    };
+
+    return (
+        <View style={{ backgroundColor: "#101218" }}>
+            <FlatList
+                showsVerticalScrollIndicator={false}
+                data={posts}
+                renderItem={({ item }) => <PostListItem post={item} />}
+            />
+        </View>
+    );
 }
